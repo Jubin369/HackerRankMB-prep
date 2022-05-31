c11dc98b0baee9bc6b9a41f11ba3dc14815bfe5d
Jubin Ayoob <jubinayoob369@gmail.com>
2022-05-31 11:10:03 +0530

    Create 7.Quicksort_1_partition.js

---
+++ 7.Quicksort_1_partition.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'quickSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function quickSort(arr,n) {
+    arr = sort(arr,0,n-1);
+    
+    return arr;
+}
+
+function sort(number,first,last){
+    let i, j, pivot, temp;
+   if(first<last){
+      pivot=first;
+      i=first;
+      j=last;
+      while(i<j){
+         while(number[i]<=number[pivot]&&i<last)
+         i++;
+         while(number[j]>number[pivot])
+         j--;
+         if(i<j){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+      temp=number[pivot];
+      number[pivot]=number[j];
+      number[j]=temp;
+      sort(number,first,j-1);
+      sort(number,j+1,last);
+      return number;
+   }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = quickSort(arr,n);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
