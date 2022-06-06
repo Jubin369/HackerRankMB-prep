02e5097a6398eeea523a50e18790568d24e34e8c
Jubin Ayoob <jubinayoob369@gmail.com>
2022-06-06 10:17:47 +0530

    Create 016.Birthday_Cake_Candles.js

---
+++ 016.Birthday_Cake_Candles.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    let greatest=0;
+    return candles.reduce((prieviousVal,currentVal)=>{
+        if(currentVal>greatest){
+            greatest = currentVal;
+            return 1;
+        }else if(currentVal==greatest){
+            
+            return prieviousVal +1;
+        }else{
+            return prieviousVal;
+        }
+    },0)
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const candlesCount = parseInt(readLine().trim(), 10);
+
+    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+    const result = birthdayCakeCandles(candles);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
