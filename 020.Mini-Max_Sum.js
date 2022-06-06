bf891e542d6f77aa4c5b4eeb23e50528d9d5bae9
Jubin Ayoob <jubinayoob369@gmail.com>
2022-06-06 16:43:09 +0530

    Create 020.Mini-Max_Sum.js

---
+++ 020.Mini-Max_Sum.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    let minSum=0,maxSum=0;
+    arr.sort();
+    for(let i=0;i<5;i++){
+        if(i!=4)
+            minSum = minSum + arr[i]
+        if(i!=0)
+            maxSum = maxSum + arr[i]
+    }
+
+    console.log(minSum+' '+maxSum);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
