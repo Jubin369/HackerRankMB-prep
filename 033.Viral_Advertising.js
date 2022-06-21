5d22b8fb7b3de8075722d30296a173c308f804e8
Jubin Ayoob <jubinayoob369@gmail.com>
2022-06-21 18:26:46 +0530

    Create 033.Viral_Advertising.js

---
+++ 033.Viral_Advertising.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function viralAdvertising(n) {
+    let result = 0;
+    
+    function totalLikes(people, n){
+        let liked = Math.floor(people / 2);
+        result += liked;
+        if(n > 1){
+            totalLikes(liked * 3, --n);
+        }
+    }
+    
+    totalLikes(5, n);
+    
+    return(result);  
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = viralAdvertising(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
